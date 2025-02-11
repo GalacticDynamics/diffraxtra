15aa23b44e71945bae670166318e0c3c9a1ada32
Nathaniel Starkman <nstarman@users.noreply.github.com>
2025-02-11 14:17:20 -0500

    ✨ feat: base class AbstractDiffeqSolver

--- src/diffraxtra/_src/__init__.py
+++ src/diffraxtra/_src/__init__.py
@@ -1,12 +1,14 @@
 """Extras for `diffrax`. Private API."""
 
 __all__ = [
+    "AbstractDiffEqSolver",
     "DiffEqSolver",
     "AbstractVectorizedDenseInterpolation",
     "VectorizedDenseInterpolation",
 ]
 
 from .diffeq import DiffEqSolver
+from .diffeq_abc import AbstractDiffEqSolver
 from .interp import (
     AbstractVectorizedDenseInterpolation,
     VectorizedDenseInterpolation,
